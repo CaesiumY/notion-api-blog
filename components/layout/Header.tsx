fabcac9ffab94ee1e9a34f4701b2ed73ca8bf976
CaesiumY <dbs2636@gmail.com>
2022-11-01 21:31:54 +0900

    헤더메뉴 추가

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,28 +1,37 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import HeaderMenu from "./HeaderMenu";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className="border-b sticky top-0 bg-white/40 backdrop-blur-md">
-      <div className="p-4 flex flex-row justify-between max-w-4xl mx-auto">
-        <button className="p-1 hover:bg-gray-200 rounded-lg">
-          <span>
-            <AiOutlineMenu size="2rem" />
-          </span>
-        </button>
-        <Link href={"/"}>
-          <h1 className="font-bold text-2xl cursor-pointer select-none">
-            Notion Blog
-          </h1>
-        </Link>
-        <button className="p-1 hover:bg-gray-200 rounded-lg">
-          <span>
-            <AiOutlineSearch size="2rem" />
-          </span>
-        </button>
-      </div>
-    </header>
+    <>
+      <header className="border-b sticky top-0 bg-white/40 backdrop-blur-md">
+        <div className="p-4 flex flex-row justify-between max-w-4xl mx-auto">
+          <button
+            className="p-1 hover:bg-gray-200 rounded-lg"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <span>
+              <AiOutlineMenu size="2rem" />
+            </span>
+          </button>
+          <Link href={"/"}>
+            <h1 className="font-bold text-2xl cursor-pointer select-none">
+              Notion Blog
+            </h1>
+          </Link>
+          <button className="p-1 hover:bg-gray-200 rounded-lg">
+            <span>
+              <AiOutlineSearch size="2rem" />
+            </span>
+          </button>
+        </div>
+      </header>
+      <HeaderMenu isMenuOpen={isMenuOpen} />
+    </>
   );
 };
 
---
+++ components/layout/HeaderMenu.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import React from "react";
+import idk from "../../public/idk.png";
+import {
+  AiOutlineHome,
+  AiOutlineTags,
+  AiOutlineSearch,
+  AiOutlineUser,
+} from "react-icons/ai";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NavTable = {
+  "/": {
+    name: "Home",
+    icon: <AiOutlineHome />,
+  },
+  "/tags": {
+    name: "Tags",
+    icon: <AiOutlineTags />,
+  },
+  "/search": {
+    name: "Search",
+    icon: <AiOutlineSearch />,
+  },
+  "/profile": {
+    name: "Profile",
+    icon: <AiOutlineUser />,
+  },
+};
+
+interface HeaderMenuProps {
+  isMenuOpen: boolean;
+}
+
+const HeaderMenu = ({ isMenuOpen }: HeaderMenuProps) => {
+  const { asPath } = useRouter();
+
+  return (
+    <aside
+      className={`bg-white fixed top-0 bottom-0 w-3/5 max-w-sm border-r transition-all duration-500 ${
+        isMenuOpen ? "left-0" : "-left-[60%]"
+      }`}
+    >
+      <div className="py-8 flex flex-col h-full">
+        <div className="relative w-full h-1/3">
+          <Image
+            src={idk}
+            alt="Profile Image"
+            objectFit="contain"
+            layout="fill"
+          />
+        </div>
+        <h1 className="text-center font-bold text-2xl">
+          <Link href="/profile">
+            <a>Caesiumy</a>
+          </Link>
+        </h1>
+
+        <ul className="mt-8 flex flex-col">
+          {Object.entries(NavTable).map(([href, value]) => (
+            <li
+              key={href}
+              className={`text-2xl text-black/70 hover:text-black hover:bg-gray-100 ${
+                asPath === href ? "text-black bg-gray-100" : ""
+              }`}
+            >
+              <Link href={href}>
+                <a className="flex flex-row gap-2 items-center p-4">
+                  <span>{value.icon}</span>
+                  {value.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default HeaderMenu;
