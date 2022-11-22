6355aea82700b43ad268fff67ce3a69a83df5bc3
CaesiumY <dbs2636@gmail.com>
2022-11-22 20:34:03 +0900

    태그별 페이지 추가

--- components/card/tags/TagItem.tsx
+++ components/card/tags/TagItem.tsx
@@ -1,5 +1,6 @@
 import { COLOR_TABLE } from "const/const";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 interface TagItemProps {
   name: string;
@@ -7,12 +8,25 @@ interface TagItemProps {
 }
 
 const TagItem = ({ name, color }: TagItemProps) => {
+  const { push, prefetch } = useRouter();
+
+  const pathToTagName = `/tags/${name.toLowerCase()}`;
+
+  const onClick = () => {
+    push(pathToTagName);
+  };
+
+  useEffect(() => {
+    prefetch(pathToTagName);
+  }, [prefetch, pathToTagName]);
+
   return (
     <button
       className="rounded-xl border px-2 py-1 hover:-translate-y-1 hover:shadow-md transition-all duration-300 font-light text-sm"
       style={{
         backgroundColor: COLOR_TABLE[color],
       }}
+      onClick={onClick}
     >
       {name}
     </button>
