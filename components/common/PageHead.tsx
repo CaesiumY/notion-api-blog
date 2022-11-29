dbccf8862ab721b48f1240ae5f5bc0ab6e0eb109
CaesiumY <dbs2636@gmail.com>
2022-11-29 21:05:09 +0900

    구글 애널리틱스 태그 추가

--- components/common/PageHead.tsx
+++ components/common/PageHead.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -33,25 +34,41 @@ const PageHead = ({
     }`;
 
   return (
-    <Head>
-      <title>{fullTitle}</title>
-      <meta name="description" content={description ?? DEFAULT_DESCRIPTION} />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="canonical" href={fullUrl} />
+    <>
+      <Head>
+        <title>{fullTitle}</title>
+        <meta name="description" content={description ?? DEFAULT_DESCRIPTION} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href={fullUrl} />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title ?? DEFAULT_TITLE} />
-      <meta
-        property="og:description"
-        content={description ?? DEFAULT_DESCRIPTION}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title ?? DEFAULT_TITLE} />
+        <meta
+          property="og:description"
+          content={description ?? DEFAULT_DESCRIPTION}
+        />
+        <meta property="og:site_name" content={title ?? DEFAULT_TITLE} />
+        <meta property="og:image" content={fullOgImageSrc} />
+        <meta property="og:image:alt" content={title ?? DEFAULT_TITLE} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content={fullUrl} />
+      </Head>
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+        strategy="afterInteractive"
       />
-      <meta property="og:site_name" content={title ?? DEFAULT_TITLE} />
-      <meta property="og:image" content={fullOgImageSrc} />
-      <meta property="og:image:alt" content={title ?? DEFAULT_TITLE} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:url" content={fullUrl} />
-    </Head>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+        `}
+      </Script>
+    </>
   );
 };
 
