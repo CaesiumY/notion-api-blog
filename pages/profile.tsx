11260d53daee100d5d3d663c1e9cc9cebe43f2a9
CaesiumY <dbs2636@gmail.com>
2022-11-29 20:21:17 +0900

    프로필 페이지 추가

---
+++ pages/profile.tsx
@@ -0,0 +1,41 @@
+import { getPageContent } from "cms/notion";
+import PageHead from "components/common/PageHead";
+import NotionPageRenderer from "components/notion/NotionPageRenderer";
+import { GetStaticProps } from "next";
+import { ExtendedRecordMap } from "notion-types";
+import React from "react";
+
+interface ProfilePageProps {
+  recordMap: ExtendedRecordMap;
+}
+
+const ProfilePage = ({ recordMap }: ProfilePageProps) => {
+  return (
+    <>
+      <PageHead title="내 프로필" />
+      <section>
+        <NotionPageRenderer recordMap={recordMap} isProfile />
+      </section>
+    </>
+  );
+};
+
+export default ProfilePage;
+
+export const getStaticProps: GetStaticProps<ProfilePageProps> = async () => {
+  const profilePageId = process.env.PROFILE_ID;
+
+  if (!profilePageId) throw new Error("PROFILE_ID is not defined");
+
+  try {
+    const recordMap = await getPageContent(profilePageId);
+
+    return {
+      props: {
+        recordMap,
+      },
+    };
+  } catch (error) {
+    throw new Error("Error while fetching profile page");
+  }
+};
