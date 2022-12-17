39e632a8c89bdc17bfe399af8c682ea0157ac44b
caesiumy <dbs2636@gmail.com>
2022-12-17 18:21:23 +0900

    chore: Add overall image unoptimization

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["s3.us-west-2.amazonaws.com", "www.notion.so"],
+    // for free plan of vercel, can't use next/image optimization anymore
+    unoptimized: true,
   },
 };
 
